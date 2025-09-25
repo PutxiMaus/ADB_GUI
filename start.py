7911891134005558eeeda94fd35243930ed151a2
PutxiMaus <106342426+PutxiMaus@users.noreply.github.com>
2025-09-25 17:09:22 +0200

    Delete start.py

--- start.py
+++
@@ -1,7 +0,0 @@
-import os, sys, subprocess
-
-base = os.path.dirname(__file__)
-tools = os.path.join(base, "tools", "platform-tools")
-os.environ["PATH"] = tools + os.pathsep + os.environ["PATH"]
-
-subprocess.run([sys.executable, "main.py"])
